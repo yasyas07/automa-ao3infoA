80dce0100b3b3d4341498f727d6762013af3c204
Yasmin <yasmin.brandao@alunos.ifsuldeminas.edu.br>
2024-02-19 10:14:25 -0300

    aula1

---
+++ aula1/exemplo2_entra.py
@@ -0,0 +1,4 @@
+n1 = input(" Digite o numero 1")
+n2 = input(" Digite o numero 2")
+#Saída
+print("Você digitou", n1,n2)
---
+++ aula1/exemplo3__processamento.py
@@ -0,0 +1,14 @@
+#Entrada
+n1 = input(" Digite o numero 1: ")
+n2 = input(" Digite o numero 2: ")
+
+#Processamento
+soma = int(n1) + int(n2)
+
+#Saída
+print("A soma é de ", n1, " + ",n2, " = ",soma)
+
+#int("10") -> converte o texto "10" para inteiro 10
+#float("1.5") -> converte o texto "1.5" para real 1.5
+#str(10) -> converte o texto "10" para texto10
+
